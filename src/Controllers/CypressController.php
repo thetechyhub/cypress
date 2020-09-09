318829541a01711f42f43ba468716d1e312c6f0c
AIMEN .S.A.SASI <aimensasi@icloud.com>
2020-09-09 11:18:23 +0800

    Add factory status and login with role

--- src/Controllers/CypressController.php
+++ src/Controllers/CypressController.php
@@ -6,59 +6,63 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
-class CypressController
-{
-    public function login(Request $request)
-    {
-        $user = factory($this->userClassName())
-            ->create($request->input('attributes', []));
+class CypressController {
+	public function login(Request $request) {
 
-        auth()->login($user);
+		$user = factory($this->userClassName())
+			->create($request->input('attributes', []));
 
-        return $user;
-    }
+		if ($request->role) {
+			$user->assignRole($request->role);
+		}
 
-    public function logout()
-    {
-        auth()->logout();
-    }
+		auth()->login($user);
 
-    public function factory(Request $request)
-    {
-        return factory($request->input('model'))
-            ->times($request->input('times'))
-            ->create($request->input('attributes'));
-    }
+		return $user;
+	}
 
-    public function artisan(Request $request)
-    {
-        Artisan::call($request->input('command'), $request->input('parameters', []));
-    }
+	public function logout() {
+		auth()->logout();
+	}
 
-    public function csrfToken()
-    {
-        return response()->json(csrf_token());
-    }
+	public function factory(Request $request) {
+		if ($request->has('state') && $request->state !== null) {
+			return factory($request->input('model'))
+				->state($request->input('state'))
+				->times($request->input('times'))
+				->create($request->input('attributes'));
+		}
 
-    public function runPhp(Request $request)
-    {
-        $code = $request->input('command');
+		return factory($request->input('model'))
+			->times($request->input('times'))
+			->create($request->input('attributes'));
+	}
 
-        if ($code[-1] !== ';') {
-            $code .= ';';
-        }
+	public function artisan(Request $request) {
+		Artisan::call($request->input('command'), $request->input('parameters', []));
+	}
 
-        if (! Str::contains($code, 'return')) {
-            $code = 'return ' . $code;
-        }
+	public function csrfToken() {
+		return response()->json(csrf_token());
+	}
 
-        return response()->json([
-            'result' => eval($code)
-        ]);
-    }
+	public function runPhp(Request $request) {
+		$code = $request->input('command');
 
-    protected function userClassName()
-    {
-        return config('auth.providers.users.model');
-    }
+		if ($code[-1] !== ';') {
+			$code .= ';';
+		}
+
+		if (!Str::contains($code, 'return')) {
+			$code = 'return ' . $code;
+		}
+
+		return response()->json([
+			'result' => eval($code)
+		]);
+	}
+
+	protected function userClassName() {
+		return config('auth.providers.users.model');
+	}
 }
